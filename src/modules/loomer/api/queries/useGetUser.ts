b06ee5447a985712edd5817a256382ee74c8b12c
ManeMaria <mirandacesar598@gmail.com>
2025-12-22 16:35:19 -0300

    feat: update index.html and add favicon and manifest files
    - Refactored index.html to include new favicon links and a web app manifest for improved PWA support.
    - Added new favicon files (favicon-96x96.png, favicon.svg, apple-touch-icon.png) to enhance branding.
    - Introduced web app manifest (site.webmanifest) for better integration with mobile devices.
    - Removed outdated README.md for the useAudioManager hook to streamline the codebase.

--- src/modules/loomer/api/queries/useGetUser.ts
+++ src/modules/loomer/api/queries/useGetUser.ts
@@ -3,7 +3,8 @@ import { getUser } from '@/modules/loomer/api'
 
 export const useGetUser = (params: { email: string }) => {
   return useQuery({
-    queryKey: ['user'],
+    queryKey: ['user', params.email],
     queryFn: () => getUser(params),
+    // enabled: !!params.email,
   })
 }
