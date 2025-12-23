348f11febb4fcbed0c0ffa54206c800413cfc001
ManeMaria <mirandacesar598@gmail.com>
2025-12-23 09:18:04 -0300

    feat: replace PNG and SVG assets with WEBP format for improved performance
    - Deleted outdated PNG and SVG files and added corresponding WEBP files for bg-app, bordainferior, bordasuperior, and border-side.
    - Updated CSS styles to reference the new WEBP images, enhancing loading times and reducing file sizes.
    - Adjusted the Border component to use 'h-dvh' for better responsiveness.

--- src/components/Border/Border.tsx
+++ src/components/Border/Border.tsx
@@ -1,6 +1,6 @@
 export const Border = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="border-effect h-screen w-full">
+    <div className="border-effect h-dvh w-full">
       <span className="border-effect-top" />
       <span className="border-effect-left" />
       <div className="overflow-y-scroll h-full w-full">{children}</div>
